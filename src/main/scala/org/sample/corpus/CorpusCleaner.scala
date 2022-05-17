3298bf458a9267099a0edeb6e53c3e9f67cfd083
hayashi_mas <hayashi_mas@worksap.co.jp>
2022-05-17 10:39:49 +0900

    add CorpusCleaner

---
+++ src/main/scala/org/sample/corpus/CorpusCleaner.scala
@@ -0,0 +1,83 @@
+package org.sample.corpus
+
+import java.nio.file.{Path, Paths}
+import org.rogach.scallop.ScallopConf
+
+import org.apache.spark.sql.{SparkSession, Dataset, DataFrame, Row}
+import org.apache.spark.sql.functions._
+
+import org.sample.corpus.cleaning._
+
+object CorpusCleaner {
+  private class Conf(args: Seq[String]) extends ScallopConf(args) {
+    val input = opt[List[Path]](required = true)
+    val output = opt[Path](default = Some(Paths.get("./out")))
+
+    val ngword = opt[Path]()
+    verify()
+  }
+
+  def run(spark: SparkSession, conf: Conf): Unit = {
+    import spark.implicits._
+
+    val raw = DocumentIO.loadRawDocuments(spark, conf.input())
+    val data = raw.as[String].map(docStr => docStr.split("\n").toSeq)
+
+    val normaliser = setupFullNormalizer()
+    val filter = setupFullFilter(conf.ngword.toOption)
+
+    val cleansed = filter
+      .filter(normaliser.normalize(data))
+      .map(doc => doc.mkString("\n"))
+      .toDF
+    DocumentIO.saveRawDocuments(cleansed, conf.output())
+  }
+
+  def setupFullNormalizer(): Normalizer = {
+    new SequenceDocumentNormalizer(
+      Seq(
+        new SequenceSentenceNormalizer(
+          Seq(
+            new CitationNormalizer,
+            new WhitespaceNormalizer
+          )
+        ),
+        new ConcatShortSentenceNormalizer
+      )
+    )
+  }
+
+  def setupFullFilter(ngwordFile: Option[Path] = None): Filter = {
+    new SequenceFilter(
+      Seq(
+        new SequenceSentenceFilter(
+          Seq(
+            new EmailFilter,
+            new UrlFilter,
+            new SentenceLengthFilter
+          )
+        ),
+        new SequenceDocumentFilter(
+          Seq(
+            new ShortDocumentFilter,
+            new ScriptFilter
+          )
+        )
+      ) ++ {
+        ngwordFile match {
+          case Some(p) => { Seq(NgWordFilter.fromFile(p)) }
+          case None    => { Seq() }
+        }
+      }
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = new Conf(args)
+    val spark =
+      SparkSession.builder().appName("CorpusCleaner").getOrCreate()
+
+    try { run(spark, conf) }
+    finally { spark.stop() }
+  }
+}
