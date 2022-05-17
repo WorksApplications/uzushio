af1910c1152175d2ae1c6a72b231e1233d54a1a6
hayashi_mas <hayashi_mas@worksap.co.jp>
2022-05-17 10:39:25 +0900

    add filter and normalizer from chitra

---
+++ src/main/scala/org/sample/corpus/cleaning/Filter.scala
@@ -0,0 +1,184 @@
+package org.sample.corpus.cleaning
+
+import collection.JavaConverters._
+import java.nio.file.Path
+import scala.io.Source
+import com.worksap.nlp.sudachi.Tokenizer
+
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.types._
+
+import org.sample.corpus.Sudachi
+
+abstract class Filter {
+  /* Filters documents in the dataset.
+   *
+   * Filtering can be per document, sentence, or word etc.
+   */
+  def filter(ds: Dataset[Seq[String]]): Dataset[Seq[String]]
+}
+
+/* Filter with multiple filters combined.
+ *
+ *  @constructor create a new filter from given filters.
+ *  @param filters filters to combine
+ */
+class SequenceFilter(filters: Seq[Filter]) extends Filter {
+  def filter(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    filters.foldLeft(ds)((ds, f) => f.filter(ds))
+  }
+}
+
+/* Filters documents that contain one of the specified words.
+ *
+ *  @constructor create a new filter with ng-word list.
+ *  @param ngwords the set of words which should not appear in the filtered documents
+ */
+class NgWordFilter(ngwords: Set[String]) extends Filter {
+  val ngwordPattern = s"""(${ngwords.mkString("|")})""".r
+  val mode = Tokenizer.SplitMode.C
+
+  def containsNgword(tok: Tokenizer, doc: Seq[String]): Boolean = {
+    for (sent <- doc) {
+      val matchIter = ngwordPattern.findAllMatchIn(sent)
+      val (matches, forSize) = matchIter.duplicate
+
+      if (forSize.size != 0) {
+        val morphmes = tok.tokenize(sent).asScala
+        val morphBegins = morphmes.map(_.begin()).toSet
+        val morphEnds = morphmes.map(_.end()).toSet
+
+        for (m <- matches) {
+          if (morphBegins.contains(m.start) && morphEnds.contains(m.end)) {
+            return true
+          }
+        }
+      }
+    }
+    false
+  }
+
+  def filter(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    import ds.sparkSession.implicits._
+
+    ds.mapPartitions(iter => {
+      // setup sudachi tokenizer per partition
+      val tok = Sudachi.setupSudachiTokenizer()
+      iter.filter(doc => !containsNgword(tok, doc))
+    })
+  }
+}
+
+object NgWordFilter {
+  def fromFile(ngwords_file: Path): NgWordFilter = {
+    val src = Source.fromFile(ngwords_file.toFile, "utf-8")
+    try {
+      val ngwords = src.getLines().map(_.trim).filter(_.nonEmpty).toSet
+      new NgWordFilter(ngwords)
+    } finally {
+      src.close
+    }
+  }
+}
+
+/* Filters documents with specific condition. */
+abstract class DocumentFilter extends Filter {
+  /* Determines if the document should be kept or not. */
+  def isFiltered(doc: Seq[String]): Boolean
+
+  override def filter(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    ds.filter(isFiltered(_))
+  }
+}
+
+/* DocumentFilter with multiple filters combined.
+ *
+ *  @constructor create a new filter from given filters.
+ *  @param filters filters to combine
+ */
+class SequenceDocumentFilter(filters: Seq[DocumentFilter])
+    extends DocumentFilter {
+  override def isFiltered(doc: Seq[String]): Boolean = {
+    // return true if all filters return ture
+    filters.foldLeft(true)((b, f) => b && f.isFiltered(doc))
+  }
+}
+
+/* Filters documents that contain script. */
+class ScriptFilter extends DocumentFilter {
+  val curlyBracketsPattern = """[\{|\}]""".r
+
+  def isFilteredSent(sent: String): Boolean = {
+    curlyBracketsPattern.findFirstIn(sent).nonEmpty
+  }
+
+  override def isFiltered(doc: Seq[String]): Boolean = {
+    doc.foldLeft(true)((b, sent) => b && isFilteredSent(sent))
+  }
+}
+
+/* Filters documents that are too short.
+ *
+ *  @constructor create a new filter.
+ *  @param min the minimum number of sentences a document should contain
+ */
+class ShortDocumentFilter(min: Int = 5) extends DocumentFilter {
+  override def isFiltered(doc: Seq[String]): Boolean = {
+    min <= doc.length
+  }
+}
+
+/* Filters sentences with specific condition. */
+abstract class SentenceFilter extends Filter {
+  /* Determines if the sentence should be kept or not. */
+  def isFiltered(sent: String): Boolean
+
+  override def filter(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    import ds.sparkSession.implicits._
+    ds.map(_.filter(isFiltered))
+  }
+}
+
+/* SentenceFilter with multiple filters combined.
+ *
+ *  @constructor create a new filter from given filters.
+ *  @param filters filters to combine
+ */
+class SequenceSentenceFilter(filters: Seq[SentenceFilter])
+    extends SentenceFilter {
+  override def isFiltered(sent: String): Boolean = {
+    // return true if all filters return ture
+    filters.foldLeft(true)((b, f) => b && f.isFiltered(sent))
+  }
+}
+
+/* Filters sentences that contain email address. */
+class EmailFilter extends SentenceFilter {
+  val emailPattern = """[\w\d_-]+@[\w\d_-]+\.[\w\d._-]+""".r
+
+  override def isFiltered(sent: String): Boolean = {
+    emailPattern.findFirstIn(sent).nonEmpty
+  }
+}
+
+/* Filters sentences that contain URL. */
+class UrlFilter extends SentenceFilter {
+  val urlPattern = """(https?|sftp?)://[\w/:%#\$&\?\(\)~\.=\+\-]+""".r
+
+  override def isFiltered(sent: String): Boolean = {
+    urlPattern.findFirstIn(sent).nonEmpty
+  }
+}
+
+/* Filters sentences that are too short or too long.
+ *
+ *  @constructor create a new filter.
+ *  @param min the minimum number of characters a sentence should contain
+ *  @param max the maximum number of characters a sentence should contain
+ */
+class SentenceLengthFilter(min: Int = 10, max: Int = 200)
+    extends SentenceFilter {
+  override def isFiltered(sent: String): Boolean = {
+    min <= sent.length && sent.length <= max
+  }
+}
---
+++ src/main/scala/org/sample/corpus/cleaning/Normalizer.scala
@@ -0,0 +1,105 @@
+package org.sample.corpus.cleaning
+
+import org.apache.spark.sql.Dataset
+
+abstract class Normalizer {
+  /* Normalizes document in the dataset. */
+  def normalize(ds: Dataset[Seq[String]]): Dataset[Seq[String]]
+}
+
+/* Normalizer with multiple ones combined.
+ *
+ *  @constructor create a new normalizer from given ones.
+ *  @param normalizers normalizers to combine
+ */
+class SequenceNormalizer(normalizers: Seq[Normalizer]) extends Normalizer {
+  override def normalize(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    normalizers.foldLeft(ds)((ds, n) => n.normalize(ds))
+  }
+}
+
+/* Normalizes document in document-wise way. */
+abstract class DocumentNormalizer extends Normalizer {
+  def normalizeDocument(doc: Seq[String]): Seq[String]
+
+  override def normalize(ds: Dataset[Seq[String]]): Dataset[Seq[String]] = {
+    import ds.sparkSession.implicits._
+    ds.map(doc => normalizeDocument(doc))
+  }
+}
+
+/* DocumentNormalizer with multiple ones combined.
+ *
+ *  @constructor create a new normalizer from given ones.
+ *  @param normalizers normalizers to combine
+ */
+class SequenceDocumentNormalizer(normalizers: Seq[DocumentNormalizer])
+    extends DocumentNormalizer {
+  override def normalizeDocument(doc: Seq[String]): Seq[String] = {
+    normalizers.foldLeft(doc)((doc, n) => n.normalizeDocument(doc))
+  }
+}
+
+/* Concat too short sentences to the previous sentence. */
+class ConcatShortSentenceNormalizer(concatThr: Int = 2)
+    extends DocumentNormalizer {
+  override def normalizeDocument(doc: Seq[String]): Seq[String] = {
+    if (doc.length <= 1) {
+      doc
+    } else {
+      val shortSentIdx = doc.zipWithIndex
+        .map(z => { if (z._1.length <= concatThr) z._2 else -1 })
+        .filter(_ > 0) // keep first sentence as is regardless of its length
+
+      val appended = shortSentIdx.reverse.foldLeft(doc)((d, i) =>
+        d.updated(i - 1, d(i - 1) + d(i))
+      )
+
+      for (i <- 0 until appended.length if (!shortSentIdx.contains(i)))
+        yield appended(i)
+    }
+  }
+}
+
+/* Normalizes document in sentence-wise way. */
+abstract class SentenceNormalizer extends DocumentNormalizer {
+  def normalizeSentence(sent: String): String
+
+  override def normalizeDocument(doc: Seq[String]): Seq[String] = {
+    doc.map(sent => normalizeSentence(sent))
+  }
+}
+
+/* SentenceNormalizer with multiple ones combined.
+ *
+ *  @constructor create a new normalizer from given ones.
+ *  @param normalizers normalizers to combine
+ */
+class SequenceSentenceNormalizer(normalizers: Seq[SentenceNormalizer])
+    extends SentenceNormalizer {
+  override def normalizeSentence(sent: String): String = {
+    normalizers.foldLeft(sent)((sent, n) => n.normalizeSentence(sent))
+  }
+}
+
+/* Removes non-printable characters and excess whitespaces. */
+class WhitespaceNormalizer extends SentenceNormalizer {
+  // todo: this may not be a good way to rm non-printable chars.
+  val nonPrintablePattern = """\p{C}""".r
+  val continuousWhitespacePattern = """\s+""".r
+
+  override def normalizeSentence(sent: String): String = {
+    val rmNP = nonPrintablePattern.replaceAllIn(sent, "")
+    continuousWhitespacePattern.replaceAllIn(rmNP, " ")
+  }
+}
+
+/* Removes citation markers.*/
+class CitationNormalizer extends SentenceNormalizer {
+  val citationPattern =
+    """\[\d+?\]|\[要.+?\]|\{\{+[^{}]+?\}\}+|\[(要出典|リンク切れ|.+?\?)\]""".r
+
+  override def normalizeSentence(sent: String): String = {
+    citationPattern.replaceAllIn(sent, "")
+  }
+}
