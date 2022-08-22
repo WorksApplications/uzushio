a35b7a72a00b25dc2ccdffccd6d9bc66ddbf5bdc
hayashi_mas <hayashi_mas@worksap.co.jp>
2022-08-22 10:33:49 +0000

    parse html with full tag

--- src/main/scala/org/sample/corpus/warc/WarcLoader.scala
+++ src/main/scala/org/sample/corpus/warc/WarcLoader.scala
@@ -10,6 +10,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 
 import org.apache.tika.parser.html.HtmlParser
+import org.apache.tika.parser.html.HtmlMapper
+import org.apache.tika.parser.ParseContext
 import org.apache.tika.metadata.Metadata
 import org.apache.tika.sax.BodyContentHandler
 
@@ -37,6 +39,41 @@ object WarcLoader {
     verify()
   }
 
+  def parseHtml(resp: HttpResponseSerializable, parser: HtmlParser) = {
+    val handler = new BodyContentHandler(
+      new ToParagraphHandler()
+      // new NWCToolkitHandler()
+    )
+
+    val meta = new Metadata()
+    resp.getHeader("Content-Type") match { // provide content-type as a hint
+      case Some(ct) => meta.add("Content-Type", ct)
+      case None     => {}
+    }
+
+    val context = new ParseContext()
+    context.set(classOf[HtmlMapper], new AllTagMapper())
+
+    val bodyIs = new ByteArrayInputStream(resp.body)
+
+    try {
+      tikaParser.parse(bodyIs, handler, meta, context)
+    } catch {
+      // case e: java.io.IOException => {}
+      case e: org.xml.sax.SAXException => { println(s"${e}") }
+      case e: org.apache.tika.exception.TikaException => {
+        println(s"${e}")
+      }
+    } finally {
+      bodyIs.close()
+    }
+
+    (
+      meta.names.map(k => (k -> Option(meta.get(k)).getOrElse(""))).toMap,
+      handler.toString
+    )
+  }
+
   def run(spark: SparkSession, conf: Conf): Unit = {
     import spark.implicits._
     val logger = LogManager.getLogger(this.getClass.getSimpleName)
@@ -55,36 +92,7 @@ object WarcLoader {
       })
       .mapPartitions(iter => {
         val tikaParser = new HtmlParser()
-
-        iter.map(resp => {
-          val handler = new BodyContentHandler(new ToParagraphHandler())
-          val meta = new Metadata()
-
-          // provide content-type as a hint
-          resp.getHeader("Content-Type") match {
-            case Some(ct) => meta.add("Content-Type", ct)
-            case None     => {}
-          }
-
-          val bodyIs = new ByteArrayInputStream(resp.body)
-
-          try {
-            tikaParser.parse(bodyIs, handler, meta)
-          } catch {
-            // case e: java.io.IOException => {}
-            case e: org.xml.sax.SAXException => { println(s"${e}") }
-            case e: org.apache.tika.exception.TikaException => {
-              println(s"${e}")
-            }
-          } finally {
-            bodyIs.close()
-          }
-
-          (
-            meta.names.map(k => (k -> Option(meta.get(k)).getOrElse(""))).toMap,
-            handler.toString
-          )
-        })
+        iter.map(resp => { parseHtml(resp, tikaParser) })
       })
       .toDF("tikaMetadata", "content")
       .limit(conf.take())
