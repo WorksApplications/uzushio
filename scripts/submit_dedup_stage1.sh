bc1e832e97b7253f4f9b3a18a80dea2d259cbaf6
Arseny Tolmachev <arseny@kotonoha.ws>
2023-08-29 07:57:09 +0900

    move params ro environment variables

--- scripts/submit_dedup_stage1.sh
+++ scripts/submit_dedup_stage1.sh
@@ -18,9 +18,16 @@ export SPARK_LOCAL_DIRS=$SGE_LOCALDIR
 SPARK_MASTER="spark://$(hostname):7077"
 "$SPARK_HOME/sbin/workers.sh" "SPARK_CONF_DIR=$UZUSHIO_ROOT/spark-config/abci-f" "$SPARK_HOME/sbin/start-worker.sh" $SPARK_MASTER
 
+# it is possible to monitor task progress with Spark UI accessible by ssh port forwarding
 echo "$(date -Iseconds) $JOB_ID ssh abci -L8080:$(hostname):8080" >> /scratch/$USER/spark-ui-monitoring
 
 mkdir -p /scratch/$USER/spark-exlog
+
+INPUT=$1
+OUTPUT=/dev/null
+CACHE=$2
+NUM_PARTITIONS=${3:-50}
+NUM_PARTITIONS_PROPAGATION=${4:-$(($NUM_PARTITIONS * 4))}
 
 "$SPARK_HOME/bin/spark-submit" \
     --class com.worksap.nlp.uzushio.main.DeduplicateParagraphs \
@@ -28,13 +35,13 @@ mkdir -p /scratch/$USER/spark-exlog
     --conf spark.driver.log.dfsDir=/scratch/$USER/spark-exlog \
     --conf spark.eventLog.dir=/scratch/$USER/spark-exlog \
     --conf spark.local.dir=$SPARK_LOCAL_DIRS \
-    $UZUSHIO_JAR \
-    --input=$1 \
-    --output=$2 \
-    --cache=$3 \
+    local://$UZUSHIO_JAR \
+    --input="$INPUT" \
+    --output="$OUTPUT" \
+    --cache="$CACHE" \
     --execution=reprHashes,stats,saveStats \
-    --propagate-partitions=400 \
-    --partitions=200
+    --propagate-partitions=$NUM_PARTITIONS_PROPAGATION \
+    --partitions=$NUM_PARTITIONS
 
 
 # python3.9 $SCRIPT_DIR/spark_on_abci.py \
