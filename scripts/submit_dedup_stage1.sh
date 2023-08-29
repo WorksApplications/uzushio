6433fe907f9d06e57e6a780c43349882c450e24e
Arseny Tolmachev <arseny@kotonoha.ws>
2023-08-29 17:30:20 +0900

    Add spark on ABCI (#8)
    * add spark on abci script
    
    * add spark on abci script
    
    * script runs
    
    * launch works
    
    * remove signals import
    
    * move working launcher to submit script
    
    * move params ro environment variables
    
    * add logic for reassembing text
    
    * corretly use param for non-filtering freq-1s
    
    * update submit script for abci

---
+++ scripts/submit_dedup_stage1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#$ -j y
+#$ -cwd
+#$ -l USE_SSH=1
+
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+UZUSHIO_ROOT=$HOME/work/uzushio
+SCRIPT_DIR=$UZUSHIO_ROOT/scripts
+export SPARK_HOME=${SPARK:-$HOME/soft/spark-3.3.2-bin-hadoop3}
+UZUSHIO_JAR=$(readlink -f "$SCRIPT_DIR/../target/scala-2.12/uzushio-assembly-0.1-SNAPSHOT.jar")
+
+export SPARK_CONF_DIR=$UZUSHIO_ROOT/spark-config/abci-f
+"$SPARK_HOME/sbin/start-master.sh"
+export SPARK_WORKERS=$SGE_JOB_HOSTLIST
+export SPARK_SSH_OPTS="-p 2222"
+export SPARK_LOCAL_DIRS=$SGE_LOCALDIR
+SPARK_MASTER="spark://$(hostname):7077"
+"$SPARK_HOME/sbin/workers.sh" "SPARK_CONF_DIR=$UZUSHIO_ROOT/spark-config/abci-f" "$SPARK_HOME/sbin/start-worker.sh" $SPARK_MASTER
+
+# it is possible to monitor task progress with Spark UI accessible by ssh port forwarding
+echo "$(date -Iseconds) $JOB_ID ssh abci -L8080:$(hostname):8080" >> /scratch/$USER/spark-ui-monitoring
+
+mkdir -p /scratch/$USER/spark-exlog
+
+INPUT=$1
+OUTPUT=$2
+CACHE=/dev/null
+NUM_PARTITIONS=${3:-50}
+NUM_PARTITIONS_PROPAGATION=${4:-$(($NUM_PARTITIONS * 4))}
+
+"$SPARK_HOME/bin/spark-submit" \
+    --class com.worksap.nlp.uzushio.main.DeduplicateParagraphs \
+    --master $SPARK_MASTER \
+    --conf spark.driver.log.dfsDir=/scratch/$USER/spark-exlog \
+    --conf spark.eventLog.dir=/scratch/$USER/spark-exlog \
+    --conf spark.local.dir=$SPARK_LOCAL_DIRS \
+    local://$UZUSHIO_JAR \
+    --input="$INPUT" \
+    --output="$OUTPUT" \
+    --execution=reprHashes,stats,saveStats \
+    --propagate-partitions=$NUM_PARTITIONS_PROPAGATION \
+    --partitions=$NUM_PARTITIONS
+
+
+# python3.9 $SCRIPT_DIR/spark_on_abci.py \
+#     --jar=$UZUSHIO_JAR --spark=$SPARK \
+#     --class=com.worksap.nlp.uzushio.main.DeduplicateParagraphs \
+#     --ping=$HOME/cc/pings \
+#     --input=$1 \
+#     --output=$2 \
+#     --cache=$3 \
+#     --execution=reprHashes,stats,saveStats \
+#     --propagate-partitions=200 \
+#     --partitions=200
+
---
+++ spark-config/abci-f/spark-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Spark environment variables for ABCI rt_F nodes
+# Correctly set temp directory and local storage to local scratch directory
+
+export SPARK_LOCAL_DIRS="$SGE_LOCALDIR"
+export SPARK_DAEMON_JAVA_OPTS="-Djava.io.tmpdir=$SGE_LOCALDIR"
+
+export SPARK_WORKER_DIR="$SGE_LOCALDIR"
+export SPARK_WORKER_OPTS="-Djava.io.tmpdir=$SGE_LOCALDIR"
+export SPARK_EXECUTOR_OPTS="-Djava.io.tmpdir=$SGE_LOCALDIR"
+
+export SPARK_LOG_DIR="/scratch/${USER:-nouser}/spark-log/${JOB_ID:-nojob}"
+mkdir -p "$SPARK_LOG_DIR"
+
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+export JAVA_HOME=/home/ace14424uu/soft/graalvm-jdk-20.0.2+9.1
