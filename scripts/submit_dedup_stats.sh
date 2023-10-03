589ae258b3c5886e3fdf7f43e5a000534ef65960
Arseny Tolmachev <arseny@kotonoha.ws>
2023-10-03 14:59:19 +0900

    add batch run script for outputting debug data

---
+++ scripts/submit_dedup_stats.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#$ -j y
+#$ -cwd
+#$ -l USE_SSH=1
+
+INPUT=$1
+OUTPUT=$2
+CACHE=$3
+NUM_PARTITIONS=${4:-50}
+NUM_PARTITIONS_PROPAGATION=${5:-$(($NUM_PARTITIONS * 4))}
+
+du -hs "$INPUT" > /dev/null &
+du -hs "$CACHE" > /dev/null &
+
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+UZUSHIO_ROOT=$HOME/work/uzushio
+SCRIPT_DIR=$UZUSHIO_ROOT/scripts
+export SPARK_HOME=${SPARK:-$HOME/soft/spark-3.4.1-bin-hadoop3}
+UZUSHIO_JAR=$(readlink -f "$SCRIPT_DIR/../core/target/scala-2.12/uzushio-assembly-0.1.0-SNAPSHOT.jar")
+
+export SPARK_CONF_DIR=$UZUSHIO_ROOT/spark-config/abci-f
+"$SPARK_HOME/sbin/start-master.sh"
+export SPARK_WORKERS=$SGE_JOB_HOSTLIST
+export SPARK_SSH_OPTS="-p 2222"
+export SPARK_LOCAL_DIRS=$SGE_LOCALDIR
+SPARK_MASTER="spark://$(hostname):7077"
+"$SPARK_HOME/sbin/workers.sh" "SPARK_CONF_DIR=$UZUSHIO_ROOT/spark-config/abci-f" "$SPARK_HOME/sbin/start-worker.sh" $SPARK_MASTER
+
+# it is possible to monitor task progress with Spark UI accessible by ssh port forwarding
+echo "$(date -Iseconds) $JOB_ID DedupFilterStatistics ssh abci -L8080:$(hostname):8080" >> /scratch/$USER/spark-ui-monitoring
+
+mkdir -p /scratch/$USER/spark-exlog
+
+"$SPARK_HOME/bin/spark-submit" \
+    --class com.worksap.nlp.uzushio.lib.runners.DedupFilterStatistics \
+    --master $SPARK_MASTER \
+    --conf spark.driver.log.dfsDir=/scratch/$USER/spark-exlog \
+    --conf spark.eventLog.dir=/scratch/$USER/spark-exlog \
+    --conf spark.local.dir=$SPARK_LOCAL_DIRS \
+    --conf spark.sql.shuffle.partitions=${NUM_PARTITIONS_PROPAGATION} \
+    local://$UZUSHIO_JAR \
+    --input="$INPUT" \
+    --stats="$CACHE" \
+    --output="$OUTPUT" --partitions=$NUM_PARTITIONS \
+    --filter=large-freq-paragraphs
+
+wait
