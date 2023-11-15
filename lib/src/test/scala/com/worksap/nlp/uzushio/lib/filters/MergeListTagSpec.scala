2dc6598e6297e7eaa0229f04ce98fd6e40aa0708
Niitsuma Takuro <niitsuma-t@asahi.com>
2023-11-15 17:57:59 +0900

    follow test codes for updating MergeListTag

--- lib/src/test/scala/com/worksap/nlp/uzushio/lib/filters/MergeListTagSpec.scala
+++ lib/src/test/scala/com/worksap/nlp/uzushio/lib/filters/MergeListTagSpec.scala
@@ -1,7 +1,6 @@
 package com.worksap.nlp.uzushio.lib.filters
 
 import com.worksap.nlp.uzushio.lib.cleaning.Document
-import com.worksap.nlp.uzushio.lib.utils.Paragraphs
 import org.scalatest.freespec.AnyFreeSpec
 
 class MergeListTagSpec extends AnyFreeSpec {
@@ -19,18 +18,18 @@ class MergeListTagSpec extends AnyFreeSpec {
     }
 
     "merge list tag texts and put 'remove' sign on rests for document including list tags" in {
-      val texts = Seq("test 1", "li test 1", "li test 2", "li test 3", "li test 4", "li test 5", "test 2")
-      val nearFreqs = Seq(1, 2, 3, 5, 4, 1, 1)
-      val exactFreqs = Seq(1, 2, 3, 5, 4, 1, 1)
-      val paths = Seq("body>p.text", "body>ul>li.text", "body>ul>li.text", "body>ul>li.text2", "body>ul>li.text2", "body>ul>li.text3", "body>p.text")
+      val texts = Seq("test 1", "li test 1", "li test 2", "li test 3", "li test 4", "li test 5", "li test 6", "option test 1", "option test 2", "test 2")
+      val nearFreqs = Seq(1, 2, 3, 5, 4, 1, 1, 1, 2, 1)
+      val exactFreqs = Seq(1, 2, 3, 5, 4, 1, 1, 1, 2, 1)
+      val paths = Seq("body>p.text", "body>ul>li.text", "body>ul>li.text", "body>ul>li.text2", "body>ul>li.text2", "body>ul>li.text2", "body>ul>li.text3", "body>datalist>option.text", "body>datalist>option.text", "body>p.text")
       val paragraphs = testParagraphs(texts, nearFreqs, exactFreqs, paths)
       val doc = Document(paragraphs)
 
-      assert(Seq("test 1", "- li test 1\n- li test 2", "- li test 3\n- li test 4", "- li test 5", "test 2") == filter.checkDocument(doc).aliveParagraphs.map(_.text).toSeq)
+      assert(Seq("test 1", "- li test 1\n- li test 2", "- li test 3\n- li test 4\n- li test 5", "li test 6", "- option test 1\n- option test 2", "test 2") == filter.checkDocument(doc).aliveParagraphs.map(_.text).toSeq)
       assert(2 == filter.checkDocument(doc).aliveParagraphs.map(_.nearFreq).drop(1).toList.head)
       assert(2 == filter.checkDocument(doc).aliveParagraphs.map(_.exactFreq).drop(1).toList.head)
-      assert(Seq(false, false, true, false, true, false, false) == filter.checkDocument(doc).paragraphs.map(_.remove != null))
-      assert(Seq("body>p.text", "body>ul>li.text", "body>ul>li.text2", "body>ul>li.text3", "body>p.text") == filter.checkDocument(doc).aliveParagraphs.map(_.path).toSeq)
+      assert(Seq(false, true, false, true, true, false, false, true, false, false) == filter.checkDocument(doc).paragraphs.map(_.remove != null))
+      assert(Seq("body>p.text", "body>ul>li.text", "body>ul>li.text2", "body>ul>li.text3", "body>datalist>option.text", "body>p.text") == filter.checkDocument(doc).aliveParagraphs.map(_.path).toSeq)
     }
   }
 }
