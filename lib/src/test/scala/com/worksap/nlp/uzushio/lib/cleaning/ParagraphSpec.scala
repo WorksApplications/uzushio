ef30449f3101229257dc66d81112e29860bfd206
Niitsuma Takuro <niitsuma-t@asahi.com>
2023-11-17 14:31:53 +0900

    add test about method of tag and css information

--- lib/src/test/scala/com/worksap/nlp/uzushio/lib/cleaning/ParagraphSpec.scala
+++ lib/src/test/scala/com/worksap/nlp/uzushio/lib/cleaning/ParagraphSpec.scala
@@ -4,9 +4,29 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class ParagraphSpec extends AnyFreeSpec {
   "Paragraph" - {
-    "return css selector strings" in {
+    "can return css selector strings" in {
       val par = Paragraph("body>p.text", "hello")
       assert(par.cssSelectors == Seq("body", "p.text"))
+    }
+
+    "can return designated tags in path without css selector" in {
+      val par = Paragraph("body>p.text", "hello")
+      assert(par.extractDescendantTag(Seq("p", "span")) == Some("p"))
+    }
+
+    "do not return designated tags in path" in {
+      val par = Paragraph("body>p.text", "hello")
+      assert(par.extractDescendantTag(Seq("span")) == None)
+    }
+
+    "can return true if the paragraph contains designated tags" in {
+      val par = Paragraph("body>p.text", "hello")
+      assert(par.containsTags(Seq("p", "span")))
+    }
+
+    "do not return true if the paragraph does not contain designated tags" in {
+      val par = Paragraph("body>p.text", "hello")
+      assert(!par.containsTags(Seq("span")))
     }
   }
 }
