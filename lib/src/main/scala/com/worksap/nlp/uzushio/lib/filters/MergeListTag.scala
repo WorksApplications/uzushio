b41af0917b04cbe21c9ce0f634d8f297c8ead14b
Takuro Niitsuma <bakednt@gmail.com>
2023-11-20 10:53:00 +0900

    develop `MarkdownizeHeading` filter (#29)
    * move code about paragraph information to Paragraph class
    
    * add test about method of tag and css information
    
    * perform scalafmt
    
    * develop MarkdownizeHeading filter
    
    ---------
    
    Co-authored-by: Niitsuma Takuro <niitsuma-t@asahi.com>

--- lib/src/main/scala/com/worksap/nlp/uzushio/lib/filters/MergeListTag.scala
+++ lib/src/main/scala/com/worksap/nlp/uzushio/lib/filters/MergeListTag.scala
@@ -7,40 +7,14 @@ class MergeListTag extends DocFilter {
   final val acceptedTags = Seq("li", "option")
   final val mdListSymbol = "- "
 
-  def containsAcceptedTag(cssSelectorStrs: Seq[String]): Boolean = {
-    extractDescendantTag(cssSelectorStrs, acceptedTags) match {
-      case Some(_) => true
-      case None => false
-    }
-  }
-
-  def extractDescendantTag(
-      cssSelectorStrs: Seq[String],
-      tagNames: Seq[String]
-  ): Option[String] = {
-    val iter = cssSelectorStrs.reverse.iterator
-    var i = 0
-
-    while (iter.hasNext) {
-      val tagWithCSS = iter.next()
-      val tagWithAttrs = tagWithCSS.split("[#\\.]")
-      i += 1
-      if (acceptedTags.contains(tagWithAttrs.head)) {
-        return Option(tagWithCSS)
-      }
-    }
-    return None
-  }
-
   override def checkDocument(doc: Document): Document = {
     var paragraphs = doc.aliveParagraphs.to[Seq]
 
     (0 until paragraphs.length - 1).foreach { i =>
       val paragraph = paragraphs(i)
       val nextParagraph = paragraphs(i + 1)
-      val isAccteptedTags = containsAcceptedTag(paragraph.cssSelectors) && containsAcceptedTag(
-        nextParagraph.cssSelectors
-      )
+      val isAccteptedTags = paragraph.containsTags(acceptedTags) && nextParagraph
+        .containsTags(acceptedTags)
 
       if (isAccteptedTags && paragraph.path == nextParagraph.path) {
         val mergedParagraph = nextParagraph.copy(
