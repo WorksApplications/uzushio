caaff5f23d699f4368ff418d86b4a53b58d226bd
Arseny Tolmachev <arseny@kotonoha.ws>
2023-12-19 15:21:03 +0900

    add tests for MergeDedupStats task

--- lib/src/main/scala/com/worksap/nlp/uzushio/lib/runners/MergeDedupStats.scala
+++ lib/src/main/scala/com/worksap/nlp/uzushio/lib/runners/MergeDedupStats.scala
@@ -3,7 +3,7 @@ package com.worksap.nlp.uzushio.lib.runners
 import com.worksap.nlp.uzushio.lib.utils.Resources.AutoClosableResource
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.LongType
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.rogach.scallop.ScallopConf
 
 object MergeDedupStats {
@@ -13,6 +13,21 @@ object MergeDedupStats {
 
     val inputData = spark.read.parquet(arg.input(): _*)
 
+    val merged = mergeStatisticDatasets(spark, inputData)
+
+    val filtered =
+      if (arg.noOnes()) {
+        merged
+      } else merged.where($"nearFreq" > 1)
+
+    val partitioned = filtered.repartition(arg.partitions(), $"hash")
+      .sortWithinPartitions($"reprHash", $"hash")
+
+    partitioned.write.option("compression", "zstd").mode(SaveMode.Overwrite).parquet(arg.output())
+  }
+
+  def mergeStatisticDatasets(spark: SparkSession, inputData: DataFrame): DataFrame = {
+    import spark.implicits._
     val clampLongToInt = udf((x: Long) => math.min(x, Int.MaxValue).toInt).asNonNullable()
 
     val combined = inputData.groupBy("hash").agg(
@@ -47,23 +62,14 @@ object MergeDedupStats {
       max($"nearFreq").as("nearFreq")
     )
 
-    val merged = correctHashes.select("hash", "reprHash", "exactFreq").join(correctFreqs, "reprHash")
-
-    val filtered =
-      if (arg.noOnes()) {
-        merged
-      } else merged.where($"nearFreq" > 1)
-
-    val partitioned = filtered.repartition(arg.partitions(), $"hash").sortWithinPartitions($"reprHash", $"hash")
-
-    partitioned.write.option("compression", "zstd").mode(SaveMode.Overwrite).parquet(arg.output())
+    correctHashes.select("hash", "reprHash", "exactFreq").join(correctFreqs, "reprHash")
   }
 
   class Args(args: Seq[String]) extends ScallopConf(args) {
     val input = opt[List[String]]()
     val output = opt[String]()
     val master = opt[String]()
-    val partitions = opt[Int](default=Some(500))
+    val partitions = opt[Int](default = Some(500))
     val noOnes = toggle(default = Some(false))
     verify()
   }
